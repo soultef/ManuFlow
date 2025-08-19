07cd5c43ea48b5b525c0fb2bfb7a18c91da3ccc1
Solomon Demisse <soul_tefera@yahoo.com>
2025-08-19 06:21:14 -0500

    MANF-8:feat(user) add UserEntity with username, password,and isActive fields with JPA mapping as well as @Column annotations.

---
+++ backend-services/user-service/src/main/java/com/soultech/user_service/entities/UserEntity.java
@@ -0,0 +1,55 @@
+package com.soultech.user_service.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
+    private Long userID;
+
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable=false)
+    private String password;
+
+    @Column(name="is_active", nullable = false)
+    private boolean isActive;
+
+    public Long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Long userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
